9ba17017c649c0e2354065215a9ff91fbf1eab08
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-12-30 17:45:18 +0200

    add logistic regression model

---
+++ code/logisticReg.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+def logisticReg(x_train, y_train, c):
+    logisticRegression = LogisticRegression(multi_class='ovr', solver='liblinear', verbose=1, C=c)
+    model = logisticRegression.fit(x_train, y_train)
+    return model
