077bc826f43d07359e90b11b964e41d24ca1f80b
mahmoodSherif <mahmoodSherif13@gmail.com>
2021-01-12 02:13:09 +0200

    add preForNew function

--- code/preprocessing/pre.py
+++ code/preprocessing/pre.py
@@ -5,20 +5,25 @@ from random import randrange
 import random
 import string
 import category_encoders as ce 
-from pandas import read_csv
+import pickle
+from pandas import read_csv, DataFrame
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MultiLabelBinarizer, minmax_scale
+from sklearn.preprocessing import MultiLabelBinarizer, MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import shuffle
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 
-def cleanArtists(songs, y_name):
+def cleanArtists(songs, y_name, path):
     '''drop rows with invalid values and destringify the list of artists'''
     songs['artists'] = songs['artists'].apply(lambda x: x[1:-1].split(', ') if(type(x) == str and len(x)) else [])
     songs['artists'] = songs['artists'].apply(lambda x: list(map(lambda y: y[1:-1], x)) )
     encoder = ce.HashingEncoder(cols=['artists'], n_components=5, return_df=True, drop_invariant=True)
     df = encoder.fit_transform(songs['artists'], songs[y_name])
+
+    # save the encoder model
+    pickle.dump(encoder, open(path+"endcoder.sav", 'wb'))
+
     songs = df.join(songs)
     return songs
 
@@ -46,6 +51,12 @@ def dropCols(songs):
 
 def pre(songs):
     y_name = songs.columns[-1]
+    
+    if y_name == "popularity_level":
+        path = "./preprocessing/models/classification/"
+    else:
+        path = "./preprocessing/models/regression/"
+        
     print("preprossing start")
     songs = cleanYear(songs)
     print("remove empty data")
@@ -55,20 +66,49 @@ def pre(songs):
     print("merge duplicates songs (might take a while) ")
     songs = mergeDuplicates(songs,y_name)
     print("clean artists (might take a while) ")
-    songsWithArtists = cleanArtists(songs, y_name)
+    songs = cleanArtists(songs, y_name, path)
     
     songs = songs.drop(columns=['name', 'artists'])
     songs.dropna(how='any',inplace=True)
-    
-    songs.iloc[:,0:-1] = minmax_scale(songs.iloc[:,0:-1])
-    
-    songsWithArtists = songsWithArtists.drop(columns=['name', 'artists'])
-    songsWithArtists.dropna(how='any',inplace=True)
+        
+    scaler = MinMaxScaler().fit(songs.iloc[:,0:-1])
+    songs.iloc[:,0:-1] = scaler.transform(songs.iloc[:,0:-1])
+    # save the encoder model
+    pickle.dump(scaler, open(path+"scaler.sav", 'wb'))
 
-    songsWithArtists.iloc[:,0:-1] = minmax_scale(songsWithArtists.iloc[:,0:-1])
+    songsWithoutAritsts = songs.loc[:,songs.columns not in ['col_0', 'col_1', 'col_2', 'col_3', 'col_4']]
     print("preprossing end")
     
-    return songs, songsWithArtists
+    return songsWithoutAritsts, songs
+
+def preForNew(songs: DataFrame):
+    y_name = songs.columns[-1]
+
+    if y_name == "popularity_level":
+        path = "./preprocessing/models/classification/"
+    else:
+        path = "./preprocessing/models/regression/"
+
+    # year
+    songs.loc[:, 'year'] = 2020 - songs.year
+    songs = songs.rename(columns={'year': 'yearsSinceCreation'})
+
+    # drop columns
+    songs = songs.drop(columns=['id', 'release_date', 'name'])
+
+    # artists
+    encoder = pickle.load(open(path + "encoder.sav", 'rb'))
+    df = encoder.transform(songs['artists'], songs[y_name])
+    songs = df.join(songs)
+    songs = songs.drop(columns=['artists'])
+    
+    # scaler
+    scaler = pickle.load(open(path + "scaler.sav", 'rb'))
+    songs.iloc[:,0:-1] = scaler.transform(songs.iloc[:,0:-1])
+
+    songsWithoutAritsts = songs.loc[:,songs.columns not in ['col_0', 'col_1', 'col_2', 'col_3', 'col_4']]
+
+    return songsWithoutAritsts, songs
 
 def runPre():
     songsRegression = read_csv("../datasets/spotify_training.csv")
