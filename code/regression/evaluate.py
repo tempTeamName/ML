10dd8c532cf46ae49a5dcae0dd8dc2555ce96a69
mahmoodSherif <mahmoodSherif13@gmail.com>
2021-01-12 02:30:21 +0200

    add test to regression evaluate

--- code/regression/evaluate.py
+++ code/regression/evaluate.py
@@ -71,3 +71,57 @@ def runEvaluate():
     print('MAE :%.3f'%metrics.mean_absolute_error(y_test, y_pred))
     print('r2 :%.3f'%metrics.r2_score(y_test, y_pred))
     print(f"=================== end {name} ===================\n\n")
+
+
+def test(songs, songsWithAritsts):
+
+    # all featurea but artists     
+    x_test = songs.iloc[:,0:-1]
+    y_test = songs.iloc[:,-1]
+    
+    model = pickle.load(open(path + "regression.sav", 'rb'))
+    degree = 4
+    name = "regression without artists"
+
+    print(f"\n\n=================== {name} ===================")
+    poly_features = PolynomialFeatures(degree=degree)
+    y_pred = model.predict(poly_features.fit_transform(x_test))
+    print('MSE :%.3f'%metrics.mean_squared_error(y_test, y_pred))
+    print('MAE :%.3f'%metrics.mean_absolute_error(y_test, y_pred))
+    print('r2 :%.3f'%metrics.r2_score(y_test, y_pred))
+    print(f"=================== end {name} ===================\n\n")
+
+    # all featurea 
+    x_test = songsWithAritsts.iloc[:,0:-1]
+    y_test = songsWithAritsts.iloc[:,-1]
+    
+    model = pickle.load(open(path + "regressionWithArtists.sav", 'rb'))
+    degree = 3
+    name = "regression with artists"
+
+    print(f"\n\n=================== {name} ===================")
+    poly_features = PolynomialFeatures(degree=degree)
+    y_pred = model.predict(poly_features.fit_transform(x_test))
+    print('MSE :%.3f'%metrics.mean_squared_error(y_test, y_pred))
+    print('MAE :%.3f'%metrics.mean_absolute_error(y_test, y_pred))
+    print('r2 :%.3f'%metrics.r2_score(y_test, y_pred))
+    print(f"=================== end {name} ===================\n\n")
+    
+
+    # with top correlated features
+    top_features = getTopCorrFeatures(songs)
+
+    x_test = songs[top_features].iloc[:,0:-1]
+    y_test = songs[top_features].iloc[:,-1]
+
+    model = pickle.load(open(path + "regressionTOP.sav", 'rb'))
+    degree = 4
+    name = "regression with top correlated features"
+
+    print(f"\n\n=================== {name} ===================")
+    poly_features = PolynomialFeatures(degree=degree)
+    y_pred = model.predict(poly_features.fit_transform(x_test))
+    print('MSE :%.3f'%metrics.mean_squared_error(y_test, y_pred))
+    print('MAE :%.3f'%metrics.mean_absolute_error(y_test, y_pred))
+    print('r2 :%.3f'%metrics.r2_score(y_test, y_pred))
+    print(f"=================== end {name} ===================\n\n")
